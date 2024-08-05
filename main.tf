cdb42c7ef529a5975b82dcd67e0ef57d5f6ef71a
Zayed Ahmed <zayed967@yahoo.com>
2024-08-05 15:36:20 -0400

    ok

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids = module.blog-sg.security_group_id
+  vpc_security_group_ids = module.blog_sg.security_group_id
 
   tags = {
     Name = "HelloWorld"
@@ -34,9 +34,10 @@ resource "aws_eip" "blog" {
   vpc      = true
 }
 
-module "blog-sg" {
+module "blog_sg" {
   source = "terraform-aws-modules/security-group/aws"
-  name = "blog-sg"
+  version = "4.13.0"
+  name = "blog"
   vpc_id = data.aws_vpc.default.id
 
   ingress_rules     = ["http-80-tcp", "https-443-tcp"]
