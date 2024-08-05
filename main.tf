94cbc23d7fe5bcef974bc76ddb83e2a637074286
Zayed Ahmed <zayed967@yahoo.com>
2024-08-05 15:11:22 -0400

    refactor

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids = [aws_security_group.blog.id]
+  vpc_security_group_ids = module.blog_new_sg.security_group_id
 
   tags = {
     Name = "HelloWorld"
@@ -34,39 +34,16 @@ resource "aws_eip" "blog" {
   vpc      = true
 }
 
-resource "aws_security_group" "blog" {
-  name = "blog-sg"
-  description = "allow http and https in. Allow everything out"
-
+module "blog_new_sg" {
+  source = "terraform-aws-modules/security-group/aws"
+  name = "blog-new-sg"
   vpc_id = data.aws_vpc.default.id
-}
 
-resource "aws_security_group_rule" "blog_http_in" {
-  type      = "ingress"
-  from_port = 80
-  to_port   = 80
-  protocol  = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.blog.id
-}
-
-resource "aws_security_group_rule" "blog_https_in" {
-  type      = "ingress"
-  from_port = 443
-  to_port   = 443
-  protocol  = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.blog.id
-}
-
-resource "aws_security_group_rule" "blog_everything_out" {
-  type      = "egress"
-  from_port = 0
-  to_port   = 0
-  protocol  = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.blog.id
+  web = {
+      ingress_rules     = ["http-80-tcp", "http-8080-tcp", "https-443-tcp"]
+      ingress_with_self = ["all-all"]
+      egress_rules      = ["all-all"]
+    }
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
 }
