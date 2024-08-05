2e3b3d82f146ab99fcf4eb22be64b39fe0478949
Zayed Ahmed <zayed967@yahoo.com>
2024-08-05 15:46:04 -0400

    hmmm

--- main.tf
+++ main.tf
@@ -22,13 +22,7 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids = {
-    type = map(list(string))
-    default = {
-      "name"     = module.blog_sg.security_group_id
-    }
-  }
-  
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
     Name = "HelloWorld"
