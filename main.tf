01228813f91cc8691a5e6ec7d53454c81f3dad73
Zayed Ahmed <zayed967@yahoo.com>
2024-08-06 14:44:06 -0400

    try this

--- main.tf
+++ main.tf
@@ -33,34 +33,10 @@ module "asg" {
   image_id        = data.aws_ami.app_ami.id
   instance_type   = var.instance_type
 
-}
+  depends_on = [
+    module.blog_elb
+  ]
 
-module "blog_sg" {
-  source = "terraform-aws-modules/security-group/aws"
-  version = "4.13.0"
-  name = "blog"
-  vpc_id = module.blog_vpc.vpc_id
-
-  ingress_rules     = ["http-80-tcp", "https-443-tcp"]
-  egress_rules      = ["all-all"]
-
-  ingress_cidr_blocks = ["0.0.0.0/0"]
-  egress_cidr_blocks = ["0.0.0.0/0"]
-}
-
-module "blog_vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-
-  name = "dev"
-  cidr = "10.0.0.0/16"
-
-  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-  tags = {
-    Terraform = "true"
-    Environment = "dev"
-  }
 }
 
 module "blog_elb" {
@@ -90,10 +66,38 @@ module "blog_elb" {
   }
 
   // ELB attachments
-  // number_of_instances = 1
+  number_of_instances = 1
   // instances           = [aws_instance.blog.id]
 
   tags = {
+    Environment = "dev"
+  }
+}
+
+module "blog_sg" {
+  source = "terraform-aws-modules/security-group/aws"
+  version = "4.13.0"
+  name = "blog"
+  vpc_id = module.blog_vpc.vpc_id
+
+  ingress_rules     = ["http-80-tcp", "https-443-tcp"]
+  egress_rules      = ["all-all"]
+
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
+}
+
+module "blog_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  tags = {
+    Terraform = "true"
     Environment = "dev"
   }
 }
