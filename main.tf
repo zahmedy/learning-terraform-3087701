46ead52bf189ae4209122004159b8e6c229533ad
Zayed Ahmed <zayed967@yahoo.com>
2024-08-06 13:10:06 -0400

    create dev vpc

--- main.tf
+++ main.tf
@@ -24,6 +24,8 @@ resource "aws_instance" "blog" {
 
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
+  subnet_id = module.blog_vpc.public_subnets[0]
+
   tags = {
     Name = "HelloWorld"
   }
@@ -38,11 +40,26 @@ module "blog_sg" {
   source = "terraform-aws-modules/security-group/aws"
   version = "4.13.0"
   name = "blog"
-  vpc_id = data.aws_vpc.default.id
+  vpc_id = module.blog_vpc.vpc_id
 
   ingress_rules     = ["http-80-tcp", "https-443-tcp"]
   egress_rules      = ["all-all"]
 
   ingress_cidr_blocks = ["0.0.0.0/0"]
   egress_cidr_blocks = ["0.0.0.0/0"]
+}
+
+module "blog_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
 }
